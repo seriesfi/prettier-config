5d9d13dd1d5892867f9cbe0004844c22cc11e750
yoonthegoon <yunis@seriesfi.com>
2025-01-07 15:27:11 -0500

    npm init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # prettier-config
+
+This package provides a shared Prettier configuration for all of our projects.
+
+## Installation
+
+```shell
+npm install --save-dev @seriesfi/prettier-config
+```
+
+## Usage
+
+Ideally, this package is paired with our [eslint-config](https://github.com/seriesfi/eslint-config) package.
+It is suggested that you set up a pre-commit hook to run Prettier on all staged files.
+The GitHub Actions workflow will fail if any files are not formatted correctly.
---
+++ prettier.config.mjs
@@ -0,0 +1,30 @@
+const prettierConfig = {
+  semi: false,
+  singleQuote: true,
+  importOrder: [
+    '^react$',
+    '^react/(.*)$',
+    // ohara-web
+    '^next$',
+    '^next/(.*)$',
+    '^@/components/(.*)$',
+    '^@/utils/(.*)$',
+    '^@/api/(.*)$',
+    // end ohara-web
+    // ohara-mobile
+    "^expo$",
+    "^expo/(.*)$",
+    // the following three import patterns were never used in ohara-mobile
+    // "^@components/(.*)$",
+    // "^@utilities/(.*)$",
+    // "^@assets/(.*)$",
+    // end ohara-mobile
+    '<THIRD_PARTY_MODULES>',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+}
+
+export default prettierConfig
